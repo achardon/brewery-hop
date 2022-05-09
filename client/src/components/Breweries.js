d5623958e5b94a6df5f93c11a65337a3153817cb
Alessandra Chardon <achardon@ucla.edu>
2022-05-09 11:03:42 -0400

    add grid for brewery results

--- client/src/components/Breweries.js
+++ client/src/components/Breweries.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import BreweryCard from './BreweryCard';
-import Container from 'react-bootstrap/Container'
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 function Breweries() {
 
     const [search, setSearch] = useState("")
     const [breweries, setBreweries] = useState("")
+
+    const breweriesToDisplay = breweries ? breweries.map((brewery) => <BreweryCard key={brewery.id} brewery={brewery} /> ) : null;
 
     function handleChange(e) {
         setSearch(e.target.value)
@@ -25,26 +29,30 @@ function Breweries() {
   return (
     <Container style={{ padding: "40px" }}>
       <h1>Search Breweries By City</h1>
-      <br/>
+      <br />
       <form onSubmit={handleSubmit}>
         <label>
           Search:
-          <input type="text" name="search" value={search} onChange={handleChange}/>
+          <input
+            type="text"
+            name="search"
+            value={search}
+            onChange={handleChange}
+          />
         </label>
         <input type="submit" value="Submit" />
       </form>
-        <br/>
-        <br/>
+      <br />
+      <br />
       <h1>Results</h1>
-      <div>{breweries? breweries.map(brewery => {
-          return (
-              <BreweryCard key={brewery.id} brewery={brewery}/>
-            // <li key={brewery.id}>
-            //   <a href={brewery.website_url}>{brewery.name}</a>
-            // </li>
-          );
-        }
-      ) : null }
+      <div className="d-flex flex-wrap">
+        <Container>
+          <Col>
+            <Row xs={1} md={3} className="g-4">
+              {breweriesToDisplay}
+            </Row>
+          </Col>
+        </Container>
       </div>
     </Container>
   );
