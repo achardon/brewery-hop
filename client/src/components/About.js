2c7ff10a83624aa5f6dce5bcb6c8ada3cda48e86
Alessandra Chardon <achardon@ucla.edu>
2022-07-07 15:34:16 -0400

    update about tab

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -12,15 +12,35 @@ function About() {
   return (
     <Container style={{ padding: "40px" }}>
       <h1>About</h1>
-      <p>This is an app that allows you to find a brewery anywhere you go!</p>
-      <Button variant="success" onClick={handleClick}>{showBeer? "Thanks!" : "A beer please!" }</Button>{" "}
-      <br/>
-      <br/>
-      {showBeer?
-      <img
-        src="http://www.brewbound.com/wp-content/uploads/2017/03/yard-house-beer-pour.jpg"
-        alt="beer"
-      /> : null }
+      <p>
+        This is an app that allows you to find a brewery anywhere you go! Go to
+        the 'Search Breweries' tab to enter a city where you would like to find
+        breweries. Once you search a city, you will see a list of all the
+        breweries in that city, as well as pins on the map to show you where
+        they are. To use the 'Bucket List' feature, or to leave a review for a
+        brewery, you need to create an account. Once signed in, you can add
+        breweries to your bucket list, you can view your breweries in the
+        'Bucket List' tab, and you can leave a review for any brewery you have
+        been to.
+      </p>
+      <p>
+        To use the 'Bucket List' feature, or to leave a review for a brewery,
+        you need to create an account. Once signed in, you can add breweries to
+        your bucket list, you can view your breweries in the 'Bucket List' tab,
+        and you can leave a review for any brewery you have been to.
+      </p>
+      <p>Happy brewery hopping!</p>
+      <Button variant="success" onClick={handleClick}>
+        {showBeer ? "Thanks!" : "A beer please!"}
+      </Button>{" "}
+      <br />
+      <br />
+      {showBeer ? (
+        <img
+          src="http://www.brewbound.com/wp-content/uploads/2017/03/yard-house-beer-pour.jpg"
+          alt="beer"
+        />
+      ) : null}
     </Container>
   );
 }
